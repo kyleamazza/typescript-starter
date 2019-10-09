a6f849c87fedbd8c30c5556c6e76f02dc518a2fc
Kyle Mazza <kyle.mazza@mobileforming.com>
2019-10-09 11:16:20 -0700

    Test linter...should fail.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,21 @@
 import convict from 'convict'; // @types/convict broken for '* as' imports.
 import * as appRoot from 'app-root-path';
 
+require('dotenv').config({
+  path: `${appRoot}/envs/.env.${process.env.NODE_ENV || 'development'}`
+});
+
 const conf = convict({
   env: {
-    doc: 'The development environment and value that process.env.NODE_ENV will take',
-    format: [ 'development', 'testing', 'qa', 'production' ],
+    doc:
+      'The development environment and value that process.env.NODE_ENV will take',
+    format: ['development', 'testing', 'qa', 'production'],
     default: 'development'
   }
 });
 
-conf.loadFile(`${appRoot}/config/${conf.get('env')}.json`);
+conf.validate({ allowed: 'strict' });
 
 const config = conf.getProperties();
 
 export { config };
-
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-console.log('Hello Starter');
+import { config } from './config';
+
+var banana;
+
+console.log(config);
 
